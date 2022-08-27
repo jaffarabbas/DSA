fbb0dffbac3b395355702838d84fda54d0207729
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-27 19:35:08 +0500

    Commited File : 1-p.cs at 8/27/2022 7:35:07 PM

---
+++ DataStructures-And-Algorithums/DataStructures-And-Algorithums/Arrays/1-p.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DataStructures_And_Algorithums.Arrays
+{
+    internal class _1_p
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            foreach (var i in arr)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
