9056efe02a30b29479f80625157eaa9e60ed1610
Jaffar Abbas <gamaportal8@gmail.com>
2023-01-14 15:52:19 +0500

    1-14-2023

--- C-Plus-DataStructures-And-Algorithums/Arrays/swap-alternate.cpp
+++ C-Plus-DataStructures-And-Algorithums/Arrays/swap-alternate.cpp
@@ -11,11 +11,13 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	cout << "Swap Alternative Of Array" << endl;
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     int size = sizeof(arr)/sizeof(arr[0]);
     for (int i = 0; i < size; i=i+2)
     {
-        swap(arr[i],arr[i+1]);
+        if(i+1 < size){
+            swap(arr[i],arr[i+1]);
+        }
     }
     for (int i = 0; i < size ; i++)
     {
