2693125dbb03c450ba7adaccbd5c7479b635c22c
Jaffar Abbas <gamaportal8@gmail.com>
2023-06-27 15:41:52 +0500

    Commited File : reverse-array.cpp at 27/06/2023 3:41:51 pm

--- C-Plus-DataStructures-And-Algorithums/Arrays/reverse-array.cpp
+++ C-Plus-DataStructures-And-Algorithums/Arrays/reverse-array.cpp
@@ -1,31 +1,43 @@
 #include <iostream>
 using namespace std;
 
-// int swap(int a , int b){
-//     int temp = 0;
-//     temp = a;
-//     a = b;
-//     b = temp;
-// }
+void reverse(int arr[],int size){
+    int s = 0;
+    int e = size - 1;
+    while(s <= e){
+    	int temp = 0;
+    	temp = arr[s];
+    	arr[s] = arr[e];
+    	arr[e] = temp;
+
+    	s++;
+    	e--;
+    }
+}
 
 int main(int argc, char const *argv[])
 {
-	cout << "Reverse Of Array" << endl;
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int sum = 0;
-    int size = sizeof(arr)/sizeof(arr[0]);
-    int start = 0;
-    int end = size - 1;
-    while (start <= end)
-    {
-        swap(arr[start],arr[end]);
-        start++;
-        end--;
-    }
-    for (int i = 0; i < size ; i++)
-    {
-        cout << arr[i] << endl;
-    }
-    
-    return 0;
+	int arr[5] = {1,2,3,4,5};
+	int arr2[4] = {1,2,3,4};
+	for (int i = 0; i < 5; ++i)
+	{
+		cout << arr[i];
+	}
+    reverse(arr,5);
+    cout << "\n";
+    for (int i = 0; i < 5; ++i)
+	{
+		cout << arr[i];
+	}
+    cout << "\n";
+	for (int i = 0; i < 4; ++i)
+	{
+		cout << arr2[i];
+	}
+    reverse(arr2,4);
+    cout << "\n";
+    for (int i = 0; i < 4; ++i)
+	{
+		cout << arr2[i];
+	}
 }
