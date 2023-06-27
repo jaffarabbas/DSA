00d2ddeff8e958c5f080de1bc9e5914569f099f5
Jaffar Abbas <gamaportal8@gmail.com>
2023-06-27 15:41:52 +0500

    Commited File : swap-alternate.cpp at 27/06/2023 3:41:52 pm

--- C-Plus-DataStructures-And-Algorithums/Arrays/swap-alternate.cpp
+++ C-Plus-DataStructures-And-Algorithums/Arrays/swap-alternate.cpp
@@ -1,23 +1,16 @@
 #include <iostream>
 using namespace std;
 
-// int swap(int a , int b){
-//     int temp = 0;
-//     temp = a;
-//     a = b;
-//     b = temp;
-// }
-
 int main(int argc, char const *argv[])
 {
     cout << "Swap Alternative Of Array" << endl;
     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     int size = sizeof(arr) / sizeof(arr[0]);
-    for (int i = 0; i < size; i = i + 2)
+    //code 
+    for (int i = 0; i < size; i +=2 )
     {
-        if (i + 1 < size)
-        {
-            swap(arr[i], arr[i + 1]);
+        if(i+1 < size){
+            swap(arr[i],arr[i+1]);
         }
     }
     for (int i = 0; i < size; i++)
